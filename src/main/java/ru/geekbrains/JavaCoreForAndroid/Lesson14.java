24fc0e79ae4f520b2d270b6b972ad05d65b60640
StDimensiy <stdimensiy@yandex.ru>
2021-02-19 16:13:13 +0300

    Методы созданы, настройки модуля тестирования выполнены.

--- src/main/java/ru/geekbrains/JavaCoreForAndroid/Lesson14.java
+++ src/main/java/ru/geekbrains/JavaCoreForAndroid/Lesson14.java
@@ -1,5 +1,8 @@
 package ru.geekbrains.JavaCoreForAndroid;
 
+
+import java.util.Arrays;
+
 /**
  * Сourse: java core for android
  * Faculty of Geek University Android Development
@@ -13,6 +16,36 @@ package ru.geekbrains.JavaCoreForAndroid;
  */
 public class Lesson14 {
     public static void main(String[] args) {
-        
+
+    }
+
+    public static int[] cutFor(int[] arr) throws RuntimeException {
+        // модуль поиска (удовлетворения основного условия - найти в массиве 4)
+        if(arr.length==0){
+            throw new RuntimeException("Пустой массив!");
+        } else {
+            for(int i = arr.length-1; i>=0; i--){
+                if(arr[i]==4){
+                    int[] resArr = new int[arr.length-i-1];
+                    for(int j=0; j<arr.length-i-1;j++){
+                        resArr[j] = arr[i+j+1];
+                    }
+                    return resArr;
+                }
+            }
+        }
+        throw new RuntimeException("Массив не содержит 4!");
+    }
+
+    public static boolean containsThreeOrFour(int[] arr) throws RuntimeException {
+        // модуль поиска (проверить наличие в массиве 4 и 1 и только их)
+        if(arr.length==0) throw new RuntimeException("Пустой массив!");
+        int elemIsFound = 0;
+        for (int j : arr) {
+            if (j != 4 && j != 1) return false;
+            if (elemIsFound == 0) elemIsFound = j;
+            if (elemIsFound != j) return true;
+        }
+        return false;
     }
 }
